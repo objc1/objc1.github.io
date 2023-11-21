91c115e44f2c3c9e26d4e55073139f73ce529cf6
objc1 <mdleypunskiy@yahoo.com>
2023-11-20 22:28:54 -0500

    Add looping through images

--- Assignment4/part2/main.js
+++ Assignment4/part2/main.js
@@ -13,14 +13,17 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 const filenames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+const fileAlts = ["Closeup of a human eye", "Rock that looks like a wave", "Purple and white pansies", "Section of wall from a pharoah's tomb", "Large moth on a leaf"];
 
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (var index = 0; index < filenames.length; ++index) {
+	const newImage = document.createElement('img');
+	newImage.setAttribute('src', fileAlts[index]);
+	newImage.setAttribute('alt', fileAlts[index]);
+	thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
