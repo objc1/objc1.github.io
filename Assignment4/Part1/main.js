0d260938da8744224db0236d868cd06ec448d9df
objc1 <mdleypunskiy@yahoo.com>
2023-11-20 21:16:11 -0500

    Add variable and function definitions

--- Assignment4/Part1/main.js
+++ Assignment4/Part1/main.js
@@ -4,3 +4,12 @@
 	Date: 20 November 2023
   Description: A fun app that generates random silly stories
 */
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
