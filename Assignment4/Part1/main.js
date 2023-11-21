b57ff1ae8e8f95a628387b945e91e3b43890b74d
objc1 <mdleypunskiy@yahoo.com>
2023-11-20 21:56:14 -0500

    Finish result function

--- Assignment4/Part1/main.js
+++ Assignment4/Part1/main.js
@@ -14,26 +14,35 @@ var insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 var insertY = ["the soup kitchen", "Disneyland", "the White House"];
 var insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+function randomValueFromArray(array) {
+	const random = Math.floor(Math.random() * array.length);
+	return array[random];
 }
 
 randomize.addEventListener('click', result);
 
 function result() {
+	var newStory = storyTex;
+	var xItem = randomValueFromArray(insertX);
+	var yItem = randomValueFromArray(insertY);
+	var zItem = randomValueFromArray(insertZ);
 
-  if(customName.value !== '') {
-    const name = customName.value;
+	newStory = newStory.replaceAll(":insertx:", xItem);
+	newStory = newStory.replaceAll(":inserty:", yItem);
+	newStory = newStory.replaceAll(":insertz:", zItem);
 
-  }
+	if (customName.value !== '') {
+		const name = customName.value;
+		newStory = newStory.replaceAll("Bob", name);
+	}
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+	if (document.getElementById("uk").checked) {
+		const weight = Math.round(300 / 14.0) + " stone";
+		const temperature = Math.round((94 - 32) * 5.0 / 9.0) + " centigrade";
+		newStory = newStory.replaceAll("94 fahrenheit", temperature);
+		newStory = newStory.replaceAll("300 pounds", weight);
+	}
 
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
+	story.textContent = newStory;
+	story.style.visibility = 'visible';
 }
